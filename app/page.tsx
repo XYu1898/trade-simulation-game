53649f1f18929163b4e3bbaa3bd6cf411111b051
Xiaoming Yu <xiaoming.happylife@gmail.com>
2025-07-16 13:31:07 +0100

    delete all

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import TradingSimulation from "@/trading-simulation"
-
-export default function Home() {
-  return (
-    <main>
-      <TradingSimulation />
-    </main>
-  )
-}
--- components/ui/use-mobile.tsx
+++
@@ -1,6 +0,0 @@
-"use client"
-import { useMediaQuery } from "@/hooks/use-mobile"
-
-export function useMobile() {
-  return useMediaQuery("(max-width: 768px)")
-}
